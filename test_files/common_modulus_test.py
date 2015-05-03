c7c0c57706b1b9e066d2e1215e5e3b0902aaa13d
osboxes.org <osboxes@osboxes.(none)>
2015-05-03 07:37:52 +0100

    Added tests

---
+++ test_files/common_modulus_test.py
@@ -0,0 +1,37 @@
+from RSAExploits import rsa_cracker
+from RSAExploits import RSAData
+from RSAExploits import rsa_obj
+from RSAExploits import TextData
+from RSAExploits import num2string
+from RSAExploits import Common_Modulus
+from Crypto.PublicKey import RSA
+import sys
+
+sys.setrecursionlimit(10000)
+
+# Parse and store all of the ciphertexts provided by the file
+rsadata_list = []
+c = None
+e = None
+N = None
+f = open("common_moduli_test.txt", 'r')
+for line in f:
+	if line.startswith("ciphertext"):
+		c = long(line.split(" ")[2], 0)
+	elif line.startswith("e"):
+		e = long(line.split(" ")[2])
+	elif line.startswith("N"):
+		N = long(line.split(" ")[2], 0)
+		rsadata_list.append(RSAData(rsa_obj(N, e), TextData(c, 1)))
+f.close()
+
+# Run the exploit by specifying it
+if Common_Modulus().run(rsadata_list):		
+    print num2string(rsadata_list[0].get_m())
+
+print "----------------------------------------------------"
+
+# Run list of exploits
+rsa_cracker.init()
+if rsa_cracker.attack(rsadata_list):
+    print num2string(rsadata_list[0].get_m())
